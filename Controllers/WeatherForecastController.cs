3a662db8e4895b4d198e122d07927bded51800f3
Juan Zurita <juanzq10dev@gmail.com>
2024-04-23 11:31:32 -0400

    Update model

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -24,76 +24,83 @@ public class WeatherForecastController : ControllerBase
 [HttpPost(Name = "Post")]
 public ShotResult Post(Shot shot)
 {
-    // Define M855 projectile out of 20 inch barrel
-    var ammo = new Ammunition(
-        weight: new Measurement<WeightUnit>(shot.ammo.weight, WeightUnit.Grain),
-        muzzleVelocity: new Measurement<VelocityUnit>(shot.ammo.muzzleVelocity, VelocityUnit.MetersPerSecond),
-        ballisticCoefficient: new BallisticCoefficient(shot.ammo.ballisticCoefficient, DragTableId.G1),
-        bulletDiameter: new Measurement<DistanceUnit>(shot.ammo.bulletDiameter, DistanceUnit.Inch),
-        bulletLength: new Measurement<DistanceUnit>(shot.ammo.bulletLength, DistanceUnit.Inch));
-
-    // Define ACOG scope
-    var sight = new Sight(
-        sightHeight: new Measurement<DistanceUnit>(shot.ammo.sightHeight, DistanceUnit.Inch),
-        verticalClick: new Measurement<AngularUnit>(0, AngularUnit.InchesPer100Yards),
-        horizontalClick: new Measurement<AngularUnit>(0, AngularUnit.InchesPer100Yards));
-
-    // M16 rifling
-    var rifling = new Rifling(
-        riflingStep: new Measurement<DistanceUnit>(12, DistanceUnit.Inch),
-        direction: TwistDirection.Right);
-
-    // Standard 100 yard ACOG zeroing
-    var zero = new ZeroingParameters(
-        distance: new Measurement<DistanceUnit>(shot.ammo.zeroDistance, DistanceUnit.Meter),
-        ammunition: null,
-        atmosphere: null);
-
-    // Define rifle by sight, zeroing, and rifling parameters
-    var rifle = new Rifle(sight: sight, zero: zero, rifling: rifling);
-
-    // Define atmosphere
-    var atmosphere = new Atmosphere(
-        pressure: new Measurement<PressureUnit>(shot.scenary.pressure, PressureUnit.InchesOfMercury),
-        pressureAtSeaLevel: true,
-        altitude: new Measurement<DistanceUnit>(shot.scenary.altitude, DistanceUnit.Foot),
-        temperature: new Measurement<TemperatureUnit>(shot.scenary.temperature, TemperatureUnit.Fahrenheit),
-        humidity: shot.scenary.humidity);
-
-    var calc = new TrajectoryCalculator();
-
-    // Shot parameters
-    var shotParams = new ShotParameters()
-    {
-        MaximumDistance = new Measurement<DistanceUnit>(shot.distance, DistanceUnit.Meter),
-        Step = new Measurement<DistanceUnit>(10, DistanceUnit.Meter),
-        // Calculate sight angle for the specified zero distance
-        SightAngle = calc.SightAngle(ammo, rifle, atmosphere),
-        CantAngle = new Measurement<AngularUnit>(shot.angle, AngularUnit.Degree),
-    };
-
-    // Define winds
-    Wind[] wind = new Wind[1]
-    {
-        new Wind()
-        {
-            Direction = new Measurement<AngularUnit>(45, AngularUnit.Degree),
-            Velocity = new Measurement<VelocityUnit>(50, VelocityUnit.KilometersPerHour),
-        },
-    };
-
-    // Calculate trajectory
-    var trajectory = calc.Calculate(ammo, rifle, atmosphere, shotParams, wind);
-    
-    // Assuming 'shot.x' is the starting position of the shot, 
-    // correct the calculation of 'x' and 'y' in the ShotResult
-    var lastPoint = trajectory.Last();
     return new ShotResult
     {
-    x = shot.x + (float) lastPoint.Drop.In(DistanceUnit.Meter),
-    y = shot.y + (float) lastPoint.Windage.In(DistanceUnit.Meter)
+        x = shot.x,
+        y = shot.y 
 
     };
+
+    // // Define M855 projectile out of 20 inch barrel
+    // var ammo = new Ammunition(
+    //     weight: new Measurement<WeightUnit>(shot.ammo.weight, WeightUnit.Grain),
+    //     muzzleVelocity: new Measurement<VelocityUnit>(shot.ammo.muzzleVelocity, VelocityUnit.MetersPerSecond),
+    //     ballisticCoefficient: new BallisticCoefficient(shot.ammo.ballisticCoefficient, DragTableId.G1),
+    //     bulletDiameter: new Measurement<DistanceUnit>(shot.ammo.bulletDiameter, DistanceUnit.Inch),
+    //     bulletLength: new Measurement<DistanceUnit>(shot.ammo.bulletLength, DistanceUnit.Inch));
+
+    // // Define ACOG scope
+    // var sight = new Sight(
+    //     sightHeight: new Measurement<DistanceUnit>(shot.ammo.sightHeight, DistanceUnit.Inch),
+    //     verticalClick: new Measurement<AngularUnit>(0, AngularUnit.InchesPer100Yards),
+    //     horizontalClick: new Measurement<AngularUnit>(0, AngularUnit.InchesPer100Yards));
+
+    // // M16 rifling
+    // var rifling = new Rifling(
+    //     riflingStep: new Measurement<DistanceUnit>(12, DistanceUnit.Inch),
+    //     direction: TwistDirection.Right);
+
+    // // Standard 100 yard ACOG zeroing
+    // var zero = new ZeroingParameters(
+    //     distance: new Measurement<DistanceUnit>(shot.ammo.zeroDistance, DistanceUnit.Meter),
+    //     ammunition: null,
+    //     atmosphere: null);
+
+    // // Define rifle by sight, zeroing, and rifling parameters
+    // var rifle = new Rifle(sight: sight, zero: zero, rifling: rifling);
+
+    // // Define atmosphere
+    // var atmosphere = new Atmosphere(
+    //     pressure: new Measurement<PressureUnit>(shot.scenary.pressure, PressureUnit.InchesOfMercury),
+    //     pressureAtSeaLevel: true,
+    //     altitude: new Measurement<DistanceUnit>(shot.scenary.altitude, DistanceUnit.Foot),
+    //     temperature: new Measurement<TemperatureUnit>(shot.scenary.temperature, TemperatureUnit.Fahrenheit),
+    //     humidity: shot.scenary.humidity);
+
+    // var calc = new TrajectoryCalculator();
+
+    // // Shot parameters
+    // var shotParams = new ShotParameters()
+    // {
+    //     MaximumDistance = new Measurement<DistanceUnit>(shot.distance, DistanceUnit.Meter),
+    //     Step = new Measurement<DistanceUnit>(10, DistanceUnit.Meter),
+    //     // Calculate sight angle for the specified zero distance
+    //     SightAngle = calc.SightAngle(ammo, rifle, atmosphere),
+    //     CantAngle = new Measurement<AngularUnit>(shot.angle, AngularUnit.Degree),
+    // };
+
+    // // Define winds
+    // Wind[] wind = new Wind[1]
+    // {
+    //     new Wind()
+    //     {
+    //         Direction = new Measurement<AngularUnit>(45, AngularUnit.Degree),
+    //         Velocity = new Measurement<VelocityUnit>(50, VelocityUnit.KilometersPerHour),
+    //     },
+    // };
+
+    // // Calculate trajectory
+    // var trajectory = calc.Calculate(ammo, rifle, atmosphere, shotParams, wind);
+    
+    // // Assuming 'shot.x' is the starting position of the shot, 
+    // // correct the calculation of 'x' and 'y' in the ShotResult
+    // var lastPoint = trajectory.Last();
+    // return new ShotResult
+    // {
+    // x = shot.x + (float) lastPoint.Drop.In(DistanceUnit.Meter),
+    // y = shot.y + (float) lastPoint.Windage.In(DistanceUnit.Meter)
+
+    // };
 }
 
     [HttpGet(Name = "GetWeatherForecast")]
--- Models/Shot.cs
+++ Models/Shot.cs
@@ -4,8 +4,8 @@ public class Shot
 {
     public float x { get; set; }
     public float y { get; set; }
-    public float distance { get; set; }
+    public float target_distance { get; set; }
     public float angle { get; set; } 
-    public Scenary scenary { get; set; }
-    public Ammo ammo { get; set; } 
+    public string scenary { get; set; }
+    // public Ammo ammo { get; set; } 
 }
